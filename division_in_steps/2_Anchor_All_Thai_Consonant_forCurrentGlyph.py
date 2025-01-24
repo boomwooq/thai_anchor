391615759a7e43fd3f3e3433c5e931393a5f837f
Boom-Promphan Suksumek <boom.promphans@gmail.com>
2025-01-24 19:08:49 +0100

    update adding anchor, to only with current glyph

--- division_in_steps/2_Anchor_All_Thai_Consonant_forCurrentGlyph.py
+++ division_in_steps/2_Anchor_All_Thai_Consonant_forCurrentGlyph.py
@@ -1,11 +1,12 @@
 ##Using Margin Pen
 from fontPens import marginPen
 from fontParts.world import RGlyph
-f = CurrentFont()
+g = CurrentGlyph()
+f = g.font
 
 
 ##First Setting
-bor_height = 460
+bor_height = 540
 baseline = 0
 
 ##Color
@@ -26,29 +27,10 @@ y_value_middle_1 = 224
 y_value_middle_2 = 150
 y_value_bottom = 0
 
-#Clear all guideline
-#f.clearGuidelines()
-for guideline in f.guidelines:
-    print (guideline)
-    if guideline.name not in ["bor-width", "popla top anchor", "tonemark_1", "tonemark_2", "tonemark_1_top", "bottom_top", "bottom_bottom"]:
-        f.removeGuideline(guideline)
-
-#make guideline
-top_guideline = f.appendGuideline((0, y_value_top), 0)
-top_guideline.showMeasurements = True
-top_guideline.name = 'top'
-
-middle_guideline_1 = f.appendGuideline((0, y_value_middle_1), 0)
-middle_guideline_1.showMeasurements = True
-middle_guideline_1.name = 'middle_1'
-
-middle_guideline_2 = f.appendGuideline((0, y_value_middle_2), 0)
-middle_guideline_2.showMeasurements = True
-middle_guideline_2.name = 'middle_2'
-
-bottom_guideline = f.appendGuideline((0, y_value_bottom), 0)
-bottom_guideline.showMeasurements = True
-bottom_guideline.name = 'bottom'
+def make_guideline(name, y_value):
+    my_guideline = f.appendGuideline((0, y_value), 0)
+    my_guideline.showMeasurements = True
+    my_guideline.name = name
 
 def margin_value_top(g, report = False):
     marginPen_top = marginPen.MarginPen(f, y_value_top)
@@ -92,55 +74,101 @@ def margin_value_bottom(g, report = False):
         print ("right margin value bottom is ", right_margin_bottom)
     return right_hit_bottom
 
+def descender_measurement(glyph_name, extra_y_offset=0):
+    if glyph_name in f and f[glyph_name].contours:
+        my_descender = f[glyph_name].bounds[1] + extra_y_offset
+    else:
+        my_descender = 0
+        print(f'descender cannot be measured if there is no {glyph_name}')
+    return my_descender
+
 def anchor_function(g, x_value_top, x_value_bottom, y_value_top, y_value_bottom, x_adjust_top=0, x_adjust_bottom=0):    
     g.clearAnchors()
     g.appendAnchor ("top", (x_value_top+x_adjust_top, y_value_top))
     g.appendAnchor ("bottom", (x_value_bottom+x_adjust_bottom, y_value_bottom))
 
+
+#Clear all guideline
+#f.clearGuidelines()
+for guideline in f.guidelines:
+    print (guideline)
+    if guideline.name not in ["bor-width", "popla top anchor", "tonemark_1", "tonemark_2", "tonemark_1_top", "bottom_top", "bottom_bottom"]:
+        f.removeGuideline(guideline)
+
+make_guideline('top', y_value_top)
+make_guideline('middle_1', y_value_middle_1)
+make_guideline('middle_2', y_value_middle_2)
+make_guideline('bottom', y_value_bottom)
+
 #top_borHeight, bottom_curve_baseline
-group_1= ["th-bobaimai", "th-sorusi", "th-moma", "th-ngongu", "th-khokhai", "th-khokhuat", "th-khorakhang", "th-yoyak", "th-chochoe", "th-yoying.less", "th-thophuthao"]
-for glyph_name in group_1: 
-    g = f[glyph_name]
-    th_anchor_top = margin_value_top(g)
-    th_anchor_bottom = margin_value_top(g)
-    anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline,0 , -10)
-    g.markColor = color_1
-    #print (margin_value_top(g, report = True))
+group_1 = ["th-bobaimai", "th-sorusi", "th-moma", "th-ngongu", "th-khokhai", "th-khokhuat", "th-khorakhang", "th-yoyak", "th-chochoe", "th-yoying.less", "th-thophuthao"]
 
 #top_borHeight_curve, bottom_baseline
 #middle_1
-group_2= ["th-kokai", "th-thothung", "th-phosamphao", "th-thothahan", "th-thonangmontho", "th-choching", "th-khokhwai", "th-khokhon", "th-sosala", "th-loling", "th-sosua"]
-for glyph_name in group_2:
-    g = f[glyph_name]
-    th_anchor_top = margin_value_middle(g, y_value_middle_1)
-    th_anchor_bottom = margin_value_bottom(g)
-    anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline,-10 , 0)
-    g.markColor = color_2
+group_2 = ["th-kokai", "th-thothung", "th-phosamphao", "th-thothahan", "th-thonangmontho", "th-choching", "th-khokhwai", "th-khokhon", "th-sosala", "th-loling", "th-sosua"]
 
 #borHeight_middle, bottom_curve_baseline
 #middle_1
 group_3 = ["th-chochang", "th-soso"]
-for glyph_name in group_3:
-    g = f[glyph_name]
+
+#borHeight_stright, top * bottom
+group_4 = ["th-nonu", "th-phophung", "th-nonen", "th-phophan"]
+
+#borHeight_middle, bottom
+#middle_2
+group_5 = ["th-hohip", "th-rorua", "th-thothong"]
+
+#borHeight_curve
+#middle_1
+group_6 = ["th-oang", "th-honokhuk", "th-chochan", "th-thothan.less", "th-wowaen", "th-lochula.short", "th-dodek", "th-totao"]
+
+#borHeight_middle, bottom
+#middle_1
+group_7 = ["th-lochula.short", "th-dodek", "th-totao"]
+
+#borHeight_tpp_curve, middle_1, short descender
+#middle_1
+group_8 = ["th-ru", "th-lu", "th-yoying", "th-dochada.short", "th-topatak.short"]
+
+#borHeight, middle_1, descender
+#middle_1
+group_9 = ["th-dochada", "th-topatak", "th-thothan"]
+
+#top_bounds, bottom
+group_10 = ["th-lochula"]
+
+group_11 = ["th-popla", "th-fofan", "th-fofa"]
+
+th_descender_short = descender_measurement('th-ru')
+th_descender = descender_measurement('th-dochada', 10)
+
+
+glyph_name = g.name
+if glyph_name in group_1:
+    th_anchor_top = margin_value_top(g)
+    th_anchor_bottom = margin_value_top(g)
+    anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0 , -10)
+    g.markColor = color_1
+    #print (margin_value_top(g, report = True))
+
+elif glyph_name in group_2:
+    th_anchor_top = margin_value_middle(g, y_value_middle_1)
+    th_anchor_bottom = margin_value_bottom(g)
+    anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, -10 , 0)
+    g.markColor = color_2
+
+elif glyph_name in group_3:
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_middle(g, y_value_middle_1)
     anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0 , -10)
     g.markColor = color_3
 
-#borHeight_stright, top * bottom
-group_4 = ["th-nonu", "th-phophung", "th-nonen", "th-phophan"]
-for glyph_name in group_4:
-    g = f[glyph_name]
+elif glyph_name in group_4:
     th_anchor_top = margin_value_top(g)
     th_anchor_bottom = margin_value_bottom(g)
     anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0 , 0)
     g.markColor = color_4
-
-#borHeight_middle, bottom
-#middle_2
-group_5 = ["th-hohip", "th-rorua", "th-thothong"]
-for glyph_name in group_5:
-    g = f[glyph_name]
+elif glyph_name in group_5:
     th_anchor_top = margin_value_middle(g, y_value_middle_2)
     th_anchor_bottom = margin_value_middle(g, y_value_middle_2)
     if g == f["th-hohip"]:
@@ -149,78 +177,52 @@ for glyph_name in group_5:
         anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0 , -10)
     g.markColor = color_5
 
-#borHeight_curve
-#middle_1
-group_6 = ["th-oang", "th-honokhuk", "th-chochan", "th-thothan.less", "th-wowaen", "th-lochula.short", "th-dodek", "th-totao"]
-for glyph_name in group_6:
-    g = f[glyph_name]
+elif glyph_name in group_6:
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_middle(g, y_value_middle_1)
     anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, -30 , -30)
     g.markColor = color_6
 
-#borHeight_middle, bottom
-#middle_1
-group_7 = ["th-lochula.short", "th-dodek", "th-totao"]
-for glyph_name in group_7:
-    g = f[glyph_name]
+elif glyph_name in group_7:
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_bottom(g)
     anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, -10, 0)
     g.markColor = color_7
 
-#borHeight_tpp_curve, middle_1, short ascender
-#middle_1
-group_8 = ["th-ru", "th-lu", "th-yoying", "th-dochada.short", "th-topatak.short"]
-#th_ascender_short is base on this glyph
-th_ascender_short = f["th-ru"].bounds[1]
-print ("group_8, thai short ascender is ", th_ascender_short)
-for glyph_name in group_8:
-    g = f[glyph_name]
+elif glyph_name in group_8:
+    print ("group_8, thai short ascender is ", th_descender_short)
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_bottom(g)
-    if g == f["th-yoying"]:
-        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_ascender_short, 0, 0)
+    if g.name == "th-yoying":
+        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_descender_short, 0, 0)
     else:
-        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_ascender_short, -10, 0)
+        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_descender_short, -10, 0)
     g.markColor = color_8
 
-#borHeight, middle_1, ascender
-#middle_1
-group_9 = ["th-dochada", "th-topatak", "th-thothan"]
-th_ascender = f["th-dochada"].bounds[1] + 10
-print ("th_ascender is", th_ascender)
-for glyph_name in group_9:
-    g = f[glyph_name]
+elif glyph_name in group_9:
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_middle(g, y_value_middle_1)
-    if g == f["th-thothan"]:
-        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_ascender, -30, 0)
+    if g.name == "th-thothan":
+        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_descender, -30, 0)
     else: 
-        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_ascender, -10, 0)
+        anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, th_descender, -10, 0)
     g.markColor = color_9
 
-#top_bounds, bottom
-group_10 = ["th-lochula"]
-for glyph_name in group_10:
-    g = f[glyph_name]
+elif glyph_name in group_10:
     th_anchor_top = margin_value_middle(g, y_value_middle_1)
     th_anchor_bottom = margin_value_bottom(g)
     #print (g.bounds)
     anchor_function(g, th_anchor_top, th_anchor_bottom, g.bounds[3], baseline , 0, 0)
 
-group_11 = ["th-popla", "th-fofan", "th-fofa"]
-#Set distance from stem, only minus value
-distance = -40
-
-for glyph_name in group_11:
-    g = f[glyph_name]
-    if g == f["th-popla"]:
-        th_anchor_top = margin_value_top_ascender(g) + distance
+elif glyph_name in group_11:
+    # Set padding from stem, only minus value
+    stem_padding = -40
+    if g.name == "th-popla":
+        th_anchor_top = margin_value_top_ascender(g) + stem_padding
         th_anchor_bottom = margin_value_top(g)
         anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0, -10)
     else:
-        th_anchor_top = margin_value_top_ascender(g) + distance
+        th_anchor_top = margin_value_top_ascender(g) + stem_padding
         th_anchor_bottom = margin_value_bottom(g)
         anchor_function(g, th_anchor_top, th_anchor_bottom, bor_height, baseline, 0, 0)
     g.markColor = color_10
